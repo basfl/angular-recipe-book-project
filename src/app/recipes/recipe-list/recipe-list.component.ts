bce6897e97c2b52bdc9e802d175a16255c3076fd
unknown <jupiter85838@yahoo.com>
2018-11-01 19:18:48 -0400

    passing data using service emitter/subscribe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,18 +12,14 @@ export class RecipeListComponent implements OnInit {
   //   new Recipe("A test Recipe", "this is simply a test !!!!", "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F07%2Fmain%2Fshrimp_fried_rice_2525601_onean_0088.jpg%3Fitok%3DZz0mHMdR&w=700&q=85"),
   //   new Recipe("Another test Recipe", "this is simply a test !!!!", "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F07%2Fmain%2Fshrimp_fried_rice_2525601_onean_0088.jpg%3Fitok%3DZz0mHMdR&w=700&q=85")
 
- // ];
- recipes: Recipe[] ;
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // ];
+  recipes: Recipe[];
 
-  constructor(private recipeService:RecipeService) { }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipes=this.recipeService.getRecipe()
-  }
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-
+    this.recipes = this.recipeService.getRecipe()
   }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService {
 
@@ -7,6 +8,7 @@ export class RecipeService {
         new Recipe("Another test Recipe", "this is simply a test !!!!", "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F07%2Fmain%2Fshrimp_fried_rice_2525601_onean_0088.jpg%3Fitok%3DZz0mHMdR&w=700&q=85")
 
     ];
+    recipeSelected=new EventEmitter<Recipe>();
     getRecipe() {
         return this.recipes.slice();
     }

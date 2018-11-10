2520eb1d7f1f9fc638b604fe4a982bfb7d553e1d
unknown <jupiter85838@yahoo.com>
2018-11-10 11:18:49 -0500

    adding new and edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipesStartComponent } from "./recipes/recipes-start/recipes-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 
 const routes: Routes = [
@@ -11,7 +12,9 @@ const routes: Routes = [
     {
         path: 'recipes', component: RecipesComponent, children: [
             { path: "", component: RecipesStartComponent },
-            { path: ":id", component: RecipeDetailComponent }
+            { path: "new", component: RecipeEditComponent },
+            { path: ":id", component: RecipeDetailComponent },
+            { path: ":id/edit", component: RecipeEditComponent }
         ]
     },
     { path: 'shopping-list', component: ShoppingListComponent },

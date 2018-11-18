ff5ab13c3c7e586cff20d3906443a7a53e6cc92f
unknown <jupiter85838@yahoo.com>
2018-11-18 18:17:28 -0500

    add form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -13,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipe: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
@@ -29,21 +29,34 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = "";
     let recipeImagePath = "";
     let recipeDescription;
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
-      const recipe = this.recipe.getRecipe(this.id);
+      const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              "name": new FormControl(ingredient.name),
+              "amount": new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
+
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients':recipeIngredients
     })
   }
-  onSubmit(){
-    console.log("-->",this.recipeForm)
+  onSubmit() {
+    console.log("-->", this.recipeForm)
   }
 
 }
